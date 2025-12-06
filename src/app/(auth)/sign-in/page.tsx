c11ce1691631072e9376178e8d225db1ae4d0e17
BhelPuriPanda <boreover582@gmail.com>
2025-12-06 10:42:10 +0530

    third commit

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      {/* Sign-in form components go here */}
+    </div>
+  );
+}
